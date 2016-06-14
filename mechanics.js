da751aea7d5ccdb512bc0e57d3d716f9e497dfab
Christian <cityfisch@yahoo.de>
2016-06-14 22:42:34 +0200

    Clean up code * Removed regexes where it was possible. * Improved parsing mechanism of webcontent.

--- mechanics.js
+++ mechanics.js
@@ -3,10 +3,14 @@ var guteFrageURL    = "";
 
 
 // Accepts a url and a callback function to run.
+// XXX: add credits
+// stolen from: http://code.tutsplus.com/tutorials/quick-tip-cross-domain-ajax-request-with-yql-and-jquery--net-10225
 function requestCrossDomain( site, callback ) {
+    
     // If no url was passed, exit.
     if ( !site ) {
-        console.log('No site was passed.');
+        // XXX: log purposes
+        console.log( 'No site was passed.' );
         return false;
     }
      
@@ -14,69 +18,67 @@ function requestCrossDomain( site, callback ) {
     var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url=\'' + site + '\'') + '&format=xml&callback=?';
 
     // Request that YSQL string, and run a callback function.
-    // Pass a defined function to prevent cache-busting.
     $.getJSON( yql, cbFunc );
     
-    function cbFunc(data) {
+    function cbFunc( data ) {
     // If we have something to work with...
     if ( data.results[0] ) {
-        // Strip out all script tags, for security reasons.
-        // BE VERY CAREFUL. This helps, but we should do more. 
-        data = data.results[0].replace(/<script[^>]*>[\s\S]*?<\/script>/gi, '');
-        
-        // If the user passed a callback, and it
-        // is a function, call it, and send through the data var.
-        if ( typeof callback === 'function') {
-            callback(data);
+    
+        if ( typeof callback === 'function' ) {
+            callback( data.results[0] );
         }
     }
+
     // Else, Maybe we requested a site that doesn't exist, and nothing returned.
-    else throw new Error('Nothing returned from getJSON.');
+    else throw new Error( 'Nothing returned from getJSON.' );
     }
 }
 
-function extractBibTex (results) {
-    
-    // $('.container').html(results);
-    // console.log(results);
 
-    var tmpdiv = document.createElement( "div" );
-    $(tmpdiv).html(results);
-    $(tmpdiv).hide();
-    $(".container").append(tmpdiv);
-    // console.log(results);
-    var question = $("div.Question");
-    var author   = question.find($('a[class="Question-author"]'))[0].text;
-    var time     = question.find(".Question-pubDate")[0].innerText;
-    var timeParts= time.split(".");
+function extractBibTex ( results ) {
+
+    // store website in dummy div element in order to parse it
+    var tmpdiv = $( '<div></div>' );
+    $(tmpdiv).html( results );
+
+    // extract information from the page
+    var question  = $( tmpdiv ).find( "div.Question" );
+
+    var author    = question.find( 'a[class="Question-author"]' )[0].text;
+
+    var time      = question.find( ".Question-pubDate" )[0].innerText;
+    var timeParts = time.split( "." );
+
     // needs to be a regular expression in order to replace EVERY occurence
-    var title    = question.find(".Question-title")[0].innerText.trim().replace(/\n/g,"");
-    var tags     = [];
-    var tagElems = $("ul.Tags-tagList").find($('li'));
+    var title     = question.find( ".Question-title" )[0].innerText.trim().replace( /\n/g,"" );
+
+    var tagElems  = $( tmpdiv ).find( "ul.Tags-tagList" ).find( 'li' );
+    var tags      = [];
     tagElems.each(function( index ) {
-      tags.push( $( this ).first().text().trim().replace(/\n/g,"") );
+      tags.push( $( this ).first().text().trim().replace( /\n/g,"" ) );
     });
     
-    string = "@article{" +  author + timeParts[2] + "\n"
-    string += "title={" + title + "},\n";
-    string += "author={" + author + "},\n";
-    string += "year={" + timeParts[2] + "},\n";
-    string += "howpublished={" + guteFrageURL + "},\n";
+    // create string representation of bibtex
+    string = "@article{"       + author + timeParts[2] +   "\n";
+    string += "title={"        + title                 + "},\n";
+    string += "author={"       + author                + "},\n";
+    string += "year={"         + timeParts[2]          + "},\n";
+    string += "howpublished={" + guteFrageURL          + "},\n";
     if (tags.length > 0){
         string += "journal={Abhandlungen zu ";
-        for (var i = 0; i < tags.length-1; i++) {
-            string += tags[i] + ", ";
+        for ( var i = 0; i < tags.length-1; i++ ) {
+            string +=            tags[i]               + ", ";
         };
-        string += tags[tags.length-1] + "}\n";
+        string +=                tags[tags.length-1]   + "}\n";
     }
-    // string += "pages={" + 42--4711 + "},\n";
     // leave comma here (never forget that !)
     string += "publisher={gutefrage.net}\n";
     string += "}\n";
 
-    var publishedString = string.replace(/\n/g, "<br>");
+    // create a html version of that string
+    var publishedString = string.replace( /\n/g, "<br>" );
 
-    $("#result").append(publishedString);
+    $( "#result" ).append( publishedString );
 
 }
 
@@ -84,17 +86,18 @@ function extractBibTex (results) {
 function initialize() {
 
     // set listener to button or textfield that extracts site
-    $("#convert").on("click", function() {
+    $( "#convert" ).on( "click", function() {
+
         // var guteFrageURL = 'http://www.gutefrage.net/frage/warum-sind-menschen-nur-so-naiv-und-dumm';
-        guteFrageURL = $("#gutefrage-url").val();
+        guteFrageURL = $( "#gutefrage-url" ).val();
 
         // check if valid site and if gutefrage.net
         // TODO
 
-        // guteFrageURL = 'http://www.gutefrage.net/frage/halloich-fange-ab-oktober-eine-zweitausbildung-an-was-kann-man-an-finanzieller-unterstuetzung-beantragen';
-        // guteFrageURL = 'http://www.gutefrage.net/frage/warum-sind-menschen-nur-so-naiv-und-dumm';
-        requestCrossDomain(guteFrageURL, extractBibTex);
-    });
+
+        requestCrossDomain( guteFrageURL, extractBibTex );
+
+    } );
     
     
 
