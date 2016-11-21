6b7691c39a7a1b42b2de9690db8e4c79fcc03e56
BobMcFry <cityfisch@yahoo.de>
2016-11-21 12:10:13 -0700

    Create working version of the app
    - Add LICENSE to every file and README.md
    - Add content to README.md (incl. disclaimer)
    - Add links to repo and github profile
    - Remove unused TODOs from code
    - Niceify code

--- README.md
+++ README.md
@@ -2,3 +2,57 @@
 The missing BibTex exporter for gutefrage.net
 
 App in action: http://bobmcfry.github.io/gutefragenet-bibtex-exporter/
+
+All images are created by [me][github].
+
+Please note: I am not associated with gutefrage.net in any way and this application was created without their knowledge.
+
+## Credits
+
+### Tools
+* [SublimeText 2][sublime]
+* [Google Chrome][chrome]
+* [Omnigraffle][omnigraffle]
+
+### Libraries...
+* [jQuery][jquery]
+* [Bootstrap][bootstrap]
+* [Yahoo Query Language][yql]
+
+## Resources
+* [gutefrage.net][gutefrage]
+
+
+## License
+The MIT License (MIT)
+
+Copyright (c) 2016 Christian Heiden
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+
+[jquery]:http://jquery.com/
+[gutefrage]:http://www.gutefrage.net/
+[bootstrap]:http://getbootstrap.com/
+[github]:https://github.com/BobMcFry
+[sublime]:http://www.sublimetext.com/2
+[chrome]:https://www.google.com/chrome/
+[yql]:https://developer.yahoo.com/yql/
+[omnigraffle]:https://www.omnigroup.com/omnigraffle
--- mechanics.js
+++ mechanics.js
@@ -1,40 +1,64 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 Christian Heiden
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 var guteFrageURL    = "";
 
-
-// Accepts a url and a callback function to run.
 // XXX: add credits
-// stolen from: http://code.tutsplus.com/tutorials/quick-tip-cross-domain-ajax-request-with-yql-and-jquery--net-10225
+// inspired by: http://code.tutsplus.com/tutorials/quick-tip-cross-domain-ajax-request-with-yql-and-jquery--net-10225
 function requestCrossDomain( site, callback ) {
     
-    // If no url was passed, exit.
+    // if there is no URL provided -> Error
     if ( !site ) {
-        // XXX: log purposes
-        console.log( 'No site was passed.' );
+        displayNoQuestion();
         return false;
     }
      
-    // Take the provided url, and add it to a YQL query. Make sure you encode it!
-    var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url=\'' + site + '\'') + '&format=xml&callback=?';
+    // Generate Query
+    var yql = 'http://query.yahooapis.com/v1/public/yql?q=' 
+                + encodeURIComponent('select * from html where url=\'' 
+                                        + site + '\'') 
+                                        + '&format=xml&callback=?';
 
-    // Request that YSQL string, and run a callback function.
-    $.getJSON( yql, cbFunc );
+    // Request that YSQL string
+    $.getJSON( yql, function( data ){
     
-    function cbFunc( data ) {
-    // If we have something to work with...
-    if ( data.results[0] ) {
-    
-        if ( typeof callback === 'function' ) {
+        if ( data.results[0] ) {
             callback( data.results[0] );
+        } else {
+            displayBadQuestion();
         }
-    }
 
-    // Else, Maybe we requested a site that doesn't exist, and nothing returned.
-    else throw new Error( 'Nothing returned from getJSON.' );
-    }
+        // Else, Maybe we requested a site that doesn't exist, and nothing returned.
+        // else throw new Error( 'Nothing returned from getJSON.' );
+
+    });
+
 }
 
 
+
+// TODO:
 function extractBibTex ( results ) {
 
     // store website in dummy div element in order to parse it
@@ -43,15 +67,11 @@ function extractBibTex ( results ) {
 
     // extract information from the page
     var question  = $( tmpdiv ).find( "div.Question" );
-
     var author    = question.find( 'a[class="Question-author"]' )[0].text;
-
     var time      = question.find( ".Question-pubDate" )[0].innerText;
     var timeParts = time.split( "." );
-
     // needs to be a regular expression in order to replace EVERY occurence
     var title     = question.find( ".Question-title" )[0].innerText.trim().replace( /\n/g,"" );
-
     var tagElems  = $( tmpdiv ).find( "ul.Tags-tagList" ).find( 'li' );
     var tags      = [];
     tagElems.each(function( index ) {
@@ -59,52 +79,103 @@ function extractBibTex ( results ) {
     });
     
     // create string representation of bibtex
-    string = "@article{"       + author + timeParts[2] +   "\n";
-    string += "title={"        + title                 + "},\n";
-    string += "author={"       + author                + "},\n";
-    string += "year={"         + timeParts[2]          + "},\n";
-    string += "howpublished={" + guteFrageURL          + "},\n";
+    // korrigiere bobtex
+    string = "@article{"       + author + timeParts[2] +   "<br>";
+    string += "    title={"        + title                 + "},<br>";
+    string += "    author={"       + author                + "},<br>";
+    string += "    year={"         + timeParts[2]          + "},<br>";
+    string += "    howpublished={" + guteFrageURL          + "},<br>";
     if (tags.length > 0){
-        string += "journal={Abhandlungen zu ";
+        string += "    journal={Abhandlungen zu ";
         for ( var i = 0; i < tags.length-1; i++ ) {
             string +=            tags[i]               + ", ";
         };
-        string +=                tags[tags.length-1]   + "}\n";
+        string +=                tags[tags.length-1]   + "}<br>";
     }
     // leave comma here (never forget that !)
-    string += "publisher={gutefrage.net}\n";
-    string += "}\n";
+    string += "    publisher={gutefrage.net}<br>";
+    string += "}<br>";
 
-    // create a html version of that string
-    var publishedString = string.replace( /\n/g, "<br>" );
 
-    $( "#result" ).append( publishedString );
+    displayPhd();
+
+    $( "#result" ).empty();
+    $( "#result" ).append( string );
 
 }
+
+
+
+function buttonListener () {
+
+    displayWebsite();
+
+    // get URL form textfield
+    guteFrageURL = $( "#gutefrage-url" ).val();
+
+    // check if valid site and if gutefrage.net
+    var urlCheck = document.createElement('a');
+    urlCheck.href = guteFrageURL;
+    if (urlCheck.hostname.toLowerCase().indexOf("gutefrage") == -1){
+        displayBadQuestion();
+    } else {
+        // console.log(urlCheck.pathname);
+        displayLogoBusy();    
+        requestCrossDomain( guteFrageURL, extractBibTex );    
+    }
+
+}
+
 
 
 function initialize() {
 
+    displayWebsite();
+    
     // set listener to button or textfield that extracts site
-    $( "#convert" ).on( "click", function() {
+    $( "#convert" ).on( "click", buttonListener);
 
-        // var guteFrageURL = 'http://www.gutefrage.net/frage/warum-sind-menschen-nur-so-naiv-und-dumm';
-        guteFrageURL = $( "#gutefrage-url" ).val();
-
-        // check if valid site and if gutefrage.net
-        // TODO
+}
 
 
-        requestCrossDomain( guteFrageURL, extractBibTex );
+// TODO:
+function displayBadQuestion(){
 
-    } );
-    
-    
+}
+
+
+// TODO:
+function displayNoQuestion(){
 
 }
 
 
 
+function displayWebsite(){
+    $("#website").css("opacity", "1.0");
+    $("#lamp_off").css("display", "block");
+    $("#lamp_glow").css("display", "none");
+    $("#lamp_on").css("display", "none");
+    $("#phd").css("opacity", "1.0");
+}
+
+
+
+function displayPhd(){
+    $("#website").css("opacity", "1.0");
+    $("#lamp_off").css("display", "none");
+    $("#lamp_glow").css("display", "none");
+    $("#lamp_on").css("display", "block");
+    $("#phd").css("opacity", "1.0");
+}
+
+function displayLogoBusy(){
+    $("#website").css("opacity", "0.5");
+    $("#lamp_off").css("display", "block");
+    $("#lamp_glow").css("display", "block");
+    $("#lamp_on").css("display", "none");
+    $("#phd").css("opacity", "0.5");
+}
 
 
 
